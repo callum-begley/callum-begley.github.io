6dda2eb92036ee48d7e449ed43dea0267dc80157
Callum Begley <cfbegley7@gmail.com>
2025-04-11 13:44:54 +1200

    add aeroplane function

---
+++ blog/script.js
@@ -0,0 +1,30 @@
+const link = document.getElementById('aeroplaneLink')
+link.addEventListener('click', fly)
+
+function fly(e) {
+  document.getElementById('aeroplane').src = '/images/woosh.jpeg'
+  link.textContent = 'Wooooooooooooooooooooooosh!'
+  e.preventDefault()
+  link.addEventListener('click', reset)
+}
+
+function reset(e) {
+  document.getElementById('aeroplane').src = '/images/how-do-planes-fly.jpg'
+  link.textContent = 'Nice landing, try again! :)'
+  e.preventDefault()
+  link.addEventListener('click', fly2)
+}
+
+function fly2(e) {
+  document.getElementById('aeroplane').src = '/images/woosh.jpeg'
+  link.textContent = 'Wooooooooooooooooooooooosh!'
+  e.preventDefault()
+  link.addEventListener('click', nofuel)
+}
+
+function nofuel(e) {
+  document.getElementById('aeroplane').src = '/images/planecrash.jpeg'
+  link.textContent = 'Sorry you crash landed :('
+  e.preventDefault()
+  link.addEventListener('click', fly2)
+}
